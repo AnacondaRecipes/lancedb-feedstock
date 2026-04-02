357dd7c75aa608def6f4e387a69c14362a01c736
Eric Lundby <Eric.Lundby@gmail.com>
2026-04-02 14:33:50 -0600

    v0.30.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,9 @@ set -euxo pipefail
 export OPENSSL_DIR=$PREFIX
 
 pushd python
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output ${SRC_DIR}/THIRDPARTY.yml
+
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
