fd502ed2e1877b47c012c1724416c30a944b4987
Andrii-Osipov <aosipov@anaconda.com>
2026-06-22 05:43:40 -0700

    lancedb 0.33.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,9 @@ for v in CFLAGS CXXFLAGS $(printenv | grep -E '^(CFLAGS|CXXFLAGS)_' | cut -d= -f
   export "$v=${!v//-O2/}"
 done
 
+# This is required to get the aarch64 builds to finish without oom errors.
+export CARGO_PROFILE_RELEASE_LTO=thin
+
 pushd python
 
 cargo-bundle-licenses \
@@ -17,3 +20,5 @@ cargo-bundle-licenses \
     --output ${SRC_DIR}/THIRDPARTY.yml
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+
+popd
